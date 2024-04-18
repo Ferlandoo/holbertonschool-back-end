3b6b673f261a3dd3be57b247c785ebc407c64e2a
Ferlando <contact@ferlando.me>
2024-04-18 12:27:42 +0200

    add ex 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+
+import requests
+import json
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(url)
+    users = response.json()
+    url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(url)
+    todos = response.json()
+    data = {}
+    for user in users:
+        data[user.get("id")] = [{
+            "username": user.get("username"),
+            "task": todo.get("title"),
+            "completed": todo.get("completed")
+        } for todo in todos if user.get("id") == todo.get("userId")]
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(data, jsonfile)
